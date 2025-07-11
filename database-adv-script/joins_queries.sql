ceada3a2f01e6aefa582fef236a6b7fe27833c89
actlikemunyao <munyaopapain@gmail.com>
2025-07-11 09:17:31 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -18,3 +18,13 @@ SELECT
     bookings.start_date
 FROM bookings
 LEFT JOIN users ON users.id = bookings.user_id;
+SELECT 
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
