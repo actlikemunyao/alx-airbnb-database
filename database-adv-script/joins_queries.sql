423557072448e35003ba0c299ee78a05de5508ee
actlikemunyao <munyaopapain@gmail.com>
2025-07-11 09:16:14 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+-- Simulated FULL OUTER JOIN
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date
+FROM bookings
+LEFT JOIN users ON users.id = bookings.user_id;
